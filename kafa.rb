563eeb6d73774dba3b74a050dfb5f8efadee45e9
ade ishs <adei@logicly.com.au>
2025-01-02 12:04:44 +1100

    Remove invalid language target zt

--- kafa.rb
+++ kafa.rb
@@ -7,7 +7,7 @@ require 'json'
 SRC_LANG = 'en'
 DEST_LANGS = %w[
   ar az bg bn ca cs da de el eo es et eu fa fi fr ga gl he hi hu id it ja
-  ko lt lv ms nb nl pl pt ro ru sk sl sq sr sv th tl tr uk ur vi zh zt
+  ko lt lv ms nb nl pl pt ro ru sk sl sq sr sv th tl tr uk ur vi zh
 ].freeze
 
 puts JSON.parse(
